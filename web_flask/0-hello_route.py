3d97555e3c0f03187320a04f0a805b45ace9e8fb
omda3310 <6197@holbertonstudents.com>
2023-09-07 16:49:27 +0200

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 
 
 app.route('/', strict_slashes=False)
+
+
 def Hello_hbnb():
     """Print Hello HBNB!"""
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=None)
+    app.run(host='0.0.0.0', port='5000', debug=True)
