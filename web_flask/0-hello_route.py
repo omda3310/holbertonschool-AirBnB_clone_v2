03da1a4a99fca46eaf19d73d6cb5cecd5118bfce
omda3310 <6197@holbertonstudents.com>
2023-09-07 16:37:14 +0200

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""Import a Flask web application
-Routers:
-      /: Displays 'Hello HBNB!'
-"""
+"""Import a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -15,5 +12,5 @@ def Hello_hbnb():
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=None)
